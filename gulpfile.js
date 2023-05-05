5c2891b1992a71a1e24cac47b8d41b700c3967a5
Roberto Hernandez <robertohdz.ti94@gmail.com>
2023-05-04 18:32:08 -0600

    Image optimizaion

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,16 @@ const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
+//Dependencies for the performance of images
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+
 //paths
 const paths = {
-    scss: 'src/scss/**/*.scss'
+    scss: 'src/scss/**/*.scss',
+    images: 'src/img/**/*.{jpg,png}'
 }
 //Functions for work with the dependencies
 
@@ -22,6 +29,39 @@ const dev = done => {
     done;
 }
 
+const versionWebp = done => {
+    const options = {
+        quality: 50
+    }
+    src( paths.images )
+    .pipe( webp(options) )
+    .pipe( dest('build/img') )
+    done()
+}
+
+const versionAvif = done => {
+    const options = {
+        quality: 50
+    }
+    src( paths.images )
+    .pipe( avif(options) )
+    .pipe( dest('build/img') )
+    done()
+}
+
+const versionJpp = done => {
+    const options = {
+        optimizationLevel: 3
+    }
+    src( paths.images )
+    .pipe( cache( imagemin(options) ) )
+    .pipe( dest('build/img') )
+    done()
+}
+
 
 exports.css = css;
 exports.dev = dev;
+exports.webp = versionWebp;
+exports.avif = versionAvif;
+exports.jpg = versionJpp;
