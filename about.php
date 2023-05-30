be9946e71bf909821731c46b9de4a512cf9d74b8
Roberto Hernandez <robertohdz.ti94@gmail.com>
2023-05-30 17:20:11 -0600

    Creating a footer templete

--- about.php
+++ about.php
@@ -98,24 +98,6 @@
         
     </main> -->
 
-    <footer>
-        <div class="footer-content">
-            <h3>Real Estate</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis commodi quia iusto porro libero.</p>
-            <ul class="socials">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-            </ul>
-            
-        </div>
-        <div class="footer-bottom">
-            <p>copyright &copy;2023 RealEstate. designed by <span>Roberto</span></p>
-        </div>
-    </footer>
-    <script src="build/js/modernizr.js"></script>
-    <script src="build/js/app.js"></script>
-</body>
-</html>
+    <?php
+        include './includes/templetes/footer.php';
+    ?>
