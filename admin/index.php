2606b587b0b3d0b030578e0115689845286846c6
Roberto Hernandez <robertohdz.ti94@gmail.com>
2023-06-08 23:25:31 -0600

    Creating table to list properties

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,32 @@
         <?php   if (intval($result) === 1) { ?>
             <p class="alert succes">Advertisement successfully created</p>
         <?php } ?>
+
+        <table class="properties">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="/Project_RealEstates/images/8561fbf710bce191736a17f8592c0882.jpg" class="image-table" alt="table-image"></td>
+                    <td>$1200000</td>
+                    <td class="flex-button">
+                        <a href="#" class="button-red">Delete</a>
+                        <a href="#" class="button-yellow">Update</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
         <div class="view-all-start">
             <a class="button_all-start" href="/Project_RealEstates/admin/properties/create.php">New Property</a>
         </div>
