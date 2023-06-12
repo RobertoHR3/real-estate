c80148ee946734467d29e30dfff06651585a7956
Roberto Hernandez <robertohdz.ti94@gmail.com>
2023-06-12 12:32:02 -0600

    Displaying property information

--- ad.php
+++ ad.php
@@ -1,39 +1,57 @@
 <?php
+    //Validate id
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+    if (!$id) {
+        header('Location: /Project_RealEstates/index.php');
+    }
+    //Import conection
+    require 'includes/config/database.php';
+    $db = dbConnection();
+    //Consult
+    $query_ad = "SELECT * FROM properties WHERE id = $id";
+    //Read a results
+    $result_ad = mysqli_query($db, $query_ad);
+    if (!$result_ad->num_rows) {
+        header('Location: /Project_RealEstates/index.php');
+    }
+    $row_ad = mysqli_fetch_assoc($result_ad);
+
     require 'includes/functions.php';
     includeTemplate('header');
 
 ?>
 
     <main class="container content-centered">
-        <h1>House for sale in front of the forest</h1>
+            <h1><?php echo $row_ad['title']; ?></h1>
 
-        <picture>
-            <source srcset="build/img/destacada.avif" type="image/avif">
-            <source srcset="build/img/destacada.webp" type="image/webp">
-            <img loading="lazy" src="build/img/destacada.jpg" alt="ad-image">
-        </picture>
+            <picture>
+                <img loading="lazy" src="images/<?php echo $row_ad['image'];?>" alt="ad-image">
+            </picture>
 
-        <div class="property_summary">
-            <p class="price">$ 3,000,000</p>
-            <ul class="features-icons">
-                <li>
-                    <img loading="lazy" src="build/img/icono_wc.svg" alt="wc-icon">
-                    <p>3</p>
-                </li>
-                <li>
-                    <img loading="lazy" src="build/img/icono_estacionamiento.svg" alt="parking-icon">
-                    <p>3</p>
-                </li>
-                <li>
-                    <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="bedroom-icon">
-                    <p>4</p>
-                </li>
-            </ul>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In sit quos corrupti cupiditate distinctio tempora assumenda labore nemo. Dignissimos voluptas cumque voluptates, dolores hic excepturi quam quisquam sapiente? Incidunt, temporibus Lorem ipsum, dolor sit amet consectetur adipisicing elit. In sit quos corrupti cupiditate distinctio tempora .</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ad assumenda inventore unde rerum maxime debitis quo reprehenderit officiis iusto accusantium tenetur sit eveniet quasi, non perferendis voluptatem explicabo laboriosam.</p>
-        </div>
+            <div class="property_summary">
+                <p class="price">$ <?php echo $row_ad['price']; ?></p>
+                <ul class="features-icons">
+                    <li>
+                        <img loading="lazy" src="build/img/icono_wc.svg" alt="wc-icon">
+                        <p><?php echo $row_ad['wc']; ?></p>
+                    </li>
+                    <li>
+                        <img loading="lazy" src="build/img/icono_estacionamiento.svg" alt="parking-icon">
+                        <p><?php echo $row_ad['parking']; ?></p>
+                    </li>
+                    <li>
+                        <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="bedroom-icon">
+                        <p><?php echo $row_ad['rooms']; ?></p>
+                    </li>
+                </ul>
+                <p><?php echo $row_ad['description']; ?></p>
+            </div>
+
+
     </main>
 
     <?php
+        mysqli_close($db);
         includeTemplate('footer');
     ?>
