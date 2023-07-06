b99e90e5a328f04cd092d0d6223c679774fd345f
Roberto Hernandez <robertohdz.ti94@gmail.com>
2023-07-05 22:14:36 -0600

    Adding OOP syntax to the home page and ads

--- ad.php
+++ ad.php
@@ -1,57 +1,47 @@
 <?php
+    require 'includes/app.php';
+    use App\Property;
     //Validate id
     $id = $_GET['id'];
     $id = filter_var($id, FILTER_VALIDATE_INT);
     if (!$id) {
         header('Location: /Project_RealEstates/index.php');
     }
-    //Import conection
-    require 'includes/app.php';
-    $db = dbConnection();
-    //Consult
-    $query_ad = "SELECT * FROM properties WHERE id = $id";
-    //Read a results
-    $result_ad = mysqli_query($db, $query_ad);
-    if (!$result_ad->num_rows) {
-        header('Location: /Project_RealEstates/index.php');
-    }
-    $row_ad = mysqli_fetch_assoc($result_ad);
-
     
+    $property = Property::find($id);
     includeTemplate('header');
 
 ?>
 
     <main class="container content-centered">
-            <h1><?php echo $row_ad['title']; ?></h1>
+            <h1><?php echo $property->title; ?></h1>
 
             <picture>
-                <img loading="lazy" src="images/<?php echo $row_ad['image'];?>" alt="ad-image">
+                <img loading="lazy" src="images/<?php echo $property->image;?>" alt="ad-image">
             </picture>
 
             <div class="property_summary">
-                <p class="price">$ <?php echo $row_ad['price']; ?></p>
+                <p class="price">$ <?php echo $property->price; ?></p>
                 <ul class="features-icons">
                     <li>
                         <img loading="lazy" src="build/img/icono_wc.svg" alt="wc-icon">
-                        <p><?php echo $row_ad['wc']; ?></p>
+                        <p><?php echo $property->wc; ?></p>
                     </li>
                     <li>
                         <img loading="lazy" src="build/img/icono_estacionamiento.svg" alt="parking-icon">
-                        <p><?php echo $row_ad['parking']; ?></p>
+                        <p><?php echo $property->parking; ?></p>
                     </li>
                     <li>
                         <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="bedroom-icon">
-                        <p><?php echo $row_ad['rooms']; ?></p>
+                        <p><?php echo $property->rooms; ?></p>
                     </li>
                 </ul>
-                <p><?php echo $row_ad['description']; ?></p>
+                <p><?php echo $property->description; ?></p>
             </div>
 
 
     </main>
 
     <?php
-        mysqli_close($db);
         includeTemplate('footer');
     ?>
