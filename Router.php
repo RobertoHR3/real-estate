ca5996461eb4038700ba1a1881e0b61f096c5f63
Roberto Hernandez <robertohdz.ti94@gmail.com>
2023-07-11 17:36:00 -0600

    Registering urls and their associated methods

--- Router.php
+++ Router.php
@@ -1,8 +1,24 @@
 <?php
     namespace MVC;
     class Router {
-        public function __construct() {
-            echo "Creating a router";
+        public $routesGet = [];
+        public $routesPost = [];
+
+        public function get($url, $fn) {
+            $this->routesGet[$url] = $fn;
+        }
+
+        public function checkUrl() {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+            $method = $_SERVER['REQUEST_METHOD'];
+
+            if ($method === 'GET') {
+                $fn = $this->routesGet[$currentUrl] ?? null;
+            }
+
+            if ($fn) {
+                call_user_func($fn, $this);
+            }
         }
     }
 ?>
--- public/index.php
+++ public/index.php
@@ -4,4 +4,8 @@
     use MVC\Router;
 
     $router = new Router();
+
+    $router->get('/ours', 'function_ours');
+    $router->get('/admin', 'function_ours');
+    $router->checkUrl();
 ?>
